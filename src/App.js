39cf85af6d993a556c0ea4ca8b9b111768ef9705
Malgorzata Wiewiora <mg.wiewiora@gmail.com>
2025-11-15 22:12:13 +0100

    Form folder created

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+import Form from "./Form";
+
+const tasks = [
+  { id: 1, content: "przejsć na Reacta", done: false },
+  { id: 2, content: "wypić kawę", done: true },
+];
+
+const hideDone = false;
+
 function App() {
   return (
     <div className="container">
@@ -6,22 +15,20 @@ function App() {
       </header>
       <main>
         <section className="section">
-          <h2 className="section__title">Dodaj nowe zadanie</h2>
+          <h2 className="section__title section__title--form">
+            Dodaj nowe zadanie
+          </h2>
           <div className="section__content">
-            <form className="form js-form">
-              <input className="form__input js-taskInput" placeholder="Co jest do zrobienia?" />
-              <button className="form__button">Dodaj nowe zadanie</button>
-            </form>
+            <Form />
           </div>
         </section>
         <section className="section">
           <div className="section__menuContainer">
             <h2 className="section__title">Lista zadań</h2>
-            <div className="menu js-menu">
-            </div>
+            <div className="menu"></div>
           </div>
           <div className="section__content">
-            <ul className="list js-taskList"></ul>
+            <ul className="list"></ul>
           </div>
         </section>
       </main>
