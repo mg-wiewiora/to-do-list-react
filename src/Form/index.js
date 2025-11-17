3876d684e561c9b5e3e9cddcf846277f2cb15cda
Malgorzata Wiewiora <mg.wiewiora@gmail.com>
2025-11-18 00:44:08 +0100

    const [taskInput, addTask] defined

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import "./style.css";
 
-const Form = () => (
-  <form className="form">
-    <input className="form__input" placeholder="Co jest do zrobienia?" />
-    <button className="form__button">Dodaj zadanie</button>
-  </form>
-);
+const Form = () => {
+  const [taskInput, addTask] = useState("");
+
+  return (
+    <form className="form">
+      <input
+        value={taskInput}
+        className="form__input"
+        placeholder="Co jest do zrobienia?"
+        onChange={({target}) => addTask(target.value)}
+      />
+      <button className="form__button">Dodaj zadanie</button>
+    </form>
+  );
+};
 
 export default Form;
