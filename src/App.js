731091630a170536051b9e5ecbea06706052e243
Malgorzata Wiewiora <mg.wiewiora@gmail.com>
2025-11-25 03:13:50 +0100

    Container styles improved

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import List from "./List";
 import Menu from "./Menu";
 import Section from "./Section";
 import Header from "./Header";
-import Container from "./Container";
+import { Container } from "./Container/styled";
 import { useTasks } from "./useTasks";
 
 function App() {
