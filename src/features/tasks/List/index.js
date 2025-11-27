a8d1fc1a957e7ea9ac85ba30a2296678ac4730d4
Malgorzata Wiewiora <mg.wiewiora@gmail.com>
2025-11-27 05:33:15 +0100

    toggleTaskDone added to tasksSlice and configured

--- src/features/tasks/List/index.js
+++ src/features/tasks/List/index.js
@@ -1,15 +1,16 @@
-import { useSelector } from "react-redux";
-import { selectTasks } from "../tasksSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { selectTasks, toggleTaskDone } from "../tasksSlice";
 import { Tasks, TaskBody, Content, Button } from "./styled";
 
-const List = ({ deleteTask, toggleTaskDone }) => {
+const List = ({ deleteTask }) => {
   const { tasks, hideDone } = useSelector(selectTasks);
+  const dispatch = useDispatch();
 
   return (
     <Tasks>
       {tasks.map((task) => (
         <TaskBody key={task.id} $hidden={task.done && hideDone}>
-          <Button $checkMark onClick={() => toggleTaskDone(task.id)}>
+          <Button $checkMark onClick={() => dispatch(toggleTaskDone(task.id))}>
             {task.done ? "✓" : ""}
           </Button>
           <Content $done={task.done}>{task.content}</Content>
