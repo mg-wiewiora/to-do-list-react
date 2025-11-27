cd6e123a98a9c3565faae111d6aa66b499435fff
Malgorzata Wiewiora <mg.wiewiora@gmail.com>
2025-11-27 03:40:41 +0100

    store.js created

---
+++ src/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import tasksReducer from "./features/tasks/tasksSlice";
+export default configureStore({
+  reducer: {
+    tasks: tasksReducer,
+  },
+});
