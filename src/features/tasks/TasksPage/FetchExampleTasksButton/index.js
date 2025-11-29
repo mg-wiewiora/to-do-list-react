e1e470ff33056d2205c7def521e3be1e745fee32
Malgorzata Wiewiora <mg.wiewiora@gmail.com>
2025-11-29 20:33:27 +0100

    FetchExampleTasksButton created as separate

---
+++ src/features/tasks/TasksPage/FetchExampleTasksButton/index.js
@@ -0,0 +1,17 @@
+import { useDispatch, useSelector } from "react-redux";
+import { fetchExampleTasks, selectLoading } from "../../tasksSlice";
+import Button from "../Button";
+
+export default () => {
+  const dispatch = useDispatch();
+  const loading = useSelector(selectLoading);
+
+  return (
+    <Button onClick={() => dispatch(fetchExampleTasks())} disabled={loading}>
+      {
+        loading 
+            ? "Ładowanie...🔄" 
+            : "Pobierz przykładowe zadania"}
+    </Button>
+  );
+};
